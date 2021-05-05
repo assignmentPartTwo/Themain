fe10b20d9e7dc300d50f53e073e04ffd8975edd4
WilliamWuforreal <83726045+WilliamWuforreal@users.noreply.github.com>
2021-05-05 14:42:02 -0400

    Add files via upload

---
+++ methodAssignmentPartTwo.java
@@ -0,0 +1,12 @@
+/*
+* Date: 2021 May 5rd
+* Name: William Wu && Ian Huang
+* Teacher: Mr.Ho
+* Description: Method summative assignment part two
+**/
+
+class methodAssignmentPartTwo {
+    public static void main(String[] args){
+
+    }    
+}
