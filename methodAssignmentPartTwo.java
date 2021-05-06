e3d046bcf38c21a2bee80a3bc7a7a53ffa73fc64
Ian Huang <349564286@gapps.yrdsb.ca>
2021-05-06 14:31:37 -0400

    the method to export the chart

--- methodAssignmentPartTwo.java
+++ methodAssignmentPartTwo.java
@@ -13,5 +13,49 @@ import java.io.IOException;
 
 class methodAssignmentPartTwo {
     public static void main(String[] args){
+        // use a for loop to identify the frequency of numbers 1- 9 appearing as the
+        // first number in the sales csv. store the integer into an array from indexes
+        // 1 to 8 (length 9).
+        // call a method that in a for loop, and parameter of the freqeuncy array, divide each
+        // integer in the indexes by 1621 or do it right after storing the frequency into the array
+        // percentage: frquency of each number divide by 1621  <-- parameter for bar graph 
 
+        // a temporary double
+        double[] deleteLater = {700, 400, 176, 80, 70, 60, 50, 45, 40};
+
+
+        // Detect for errors when generating the results file
+        try {
+            exportChart(deleteLater);   // call exportChart method
+        }
+        catch (Exception e) {
+            System.out.println("File error");
+        }
+    }
+    /**
+     * Description: output the percentage of each number as a chart in a new
+     *              csv file.
+     * @author Ian
+     * @param csvLine       the array holding the percentage of how often each number appears
+     * @throws IOException  if input or output exception has occured when appending on a file
+     */
+    public static void exportChart(double[] csvLine) throws IOException {
+        File outFile = new File("results.csv");    // creating a csv file
+
+        FileWriter fr = new FileWriter(outFile, true);  // boolean to append
+        BufferedWriter br = new BufferedWriter(fr);     // initialize buffer writer
+        PrintWriter out = new PrintWriter(br);      // let printwriter access the file
+
+        for (int i = 1; i < 10; i++) {
+            out.write(i + " = " + csvLine[i-1] + "%");      // editing the file
+            out.write("\n");
+        }
+
+        // provides the location and name of the file
+        System.out.println("File located at: " + outFile.getAbsolutePath() + "\nName: results.csv");
+        System.out.println("\nDone\n");
+        out.close();    // closing print writer
+        br.close();     // closing buffer writer
+        fr.close();     // closing file writer
+    }
 }
